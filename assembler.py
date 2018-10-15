9fc558b2c0bc3365d8fe7bf993453e766a279aac
Dikshant Sagar <sagar.dikshant@gmail.com>
2018-10-15 23:37:34 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -8,7 +8,7 @@ opcode = {
             'SAC' : '0010',
             'ADD' : '0011',
             'SUB' : '0100',
-            'BRZ' : '0101',
+            'BRZ' : '0101',                               #opcode dictionary
             'BRP' : '0111',
             'INP' : '1000',
             'DSP' : '1001',
@@ -22,7 +22,7 @@ opcode = {
 file=open('input.txt','r')
 lines=[]
 for x in file:
-    a=x.splitlines()[0]
+    a=x.splitlines()[0]                 # reading lines from the files
     a=a.split(" ")
     z=[]
     for s in a:
@@ -41,8 +41,15 @@ memorycounter=1
 memory={}
 
 for k in range(len(lines)):
+    for i in range(len(lines[k])):                 
+        if(lines[k][i]=='DS'):
+            memory[lines[k][i-1]]=[get_bin(memorycounter,4),int(lines[k][i+1])]   # detecting variable declaration and allocating virtual memory address
+            memorycounter+=1
+            lines[k][i+1]=get_bin(int(lines[k][i+1]),4)
+            
+for k in range(len(lines)):
     for i in range(len(lines[k])):
-        if(lines[k][i] in memory):
+        if(lines[k][i] in memory):                       # replacing variables with allocated addresses.
             lines[k][i]=memory[lines[k][i]][0]
 
 
